09a1c93d6dd88ee3a1770e864e725dcb5bcdb0a9
jahidhasanmonnaostad <jahidhasanmonna8@gmail.com>
2024-11-10 23:21:00 +0600

    ostadAssingment

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:"Ostad Flutter Project ",
+      debugShowCheckedModeBanner: false,
+      home:HomeActivity(),
+    );
+  }
+
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Profile"),
+        backgroundColor: Colors.yellow,
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.call)),
+        ],
+      ),
+      body:Center(
+        child:Column(
+          mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+          children: [
+            CircleAvatar(
+              radius:60,
+              backgroundColor:Colors.grey,
+              child:IconButton(onPressed: (){}, icon:Icon(Icons.icecream)),
+            ),
+            Text("Ice cream is very delicious right?"),
+
+            CircleAvatar(
+              radius:60,
+              backgroundColor:Colors.grey,
+              child:IconButton(onPressed: (){}, icon:Icon(Icons.code)),
+            ),
+            Text("Programing is not boring if you love"),
+
+            CircleAvatar(
+              radius:60,
+              backgroundColor:Colors.grey,
+              child:IconButton(onPressed: (){}, icon:Icon(Icons.water_drop)),
+            ),
+            Text("Sir i  am not use chatgpt "),
+
+
+
+
+          ],
+
+        ),
+      ),
+    );
+  }
+
+}
